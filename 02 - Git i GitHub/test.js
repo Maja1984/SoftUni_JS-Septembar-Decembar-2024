9062e90c793beeddaa6e5a120c97da5d736b1aaf
Maja1984 <stankovic.maja1984@gmail.com>
2024-09-18 12:16:56 +0200

    Merge branch 'main' of https://github.com/maha1984SoftUni_JS-Septembar-Decembar-2024

--- 02 - Git i GitHub/test.js
+++ 02 - Git i GitHub/test.js
@@ -1,4 +1,4 @@
 console.log("Hello world");
 console.log("Druga linija koda");
 console.log("Treca linija koda iz Git-a");
-console.log("Treca linija koda iz GitHub-a);
+console.log("Treca linija koda iz GitHub-a");
